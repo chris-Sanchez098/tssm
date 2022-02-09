709bf33cebd62721e0ff346c6dd0a551e3d73452
Victor <victor.sapuyes@correounivalle.edu.co>
2022-02-08 19:48:36 -0500

    Maven y JDBC
    Integrar dependecia jdbc, activar jdnc in module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module tims {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    //requires org.postgresql.jdbc;
+    requires org.postgresql.jdbc;
 
     opens tssm to javafx.fxml;
     exports tssm;
