8219d8e8555238afdc16fdf705d3ea3b49ac2626
Chris Sanchez <christian.valenzuela@correounivalle.edu.co>
2022-02-12 02:45:41 -0500

    Evento create
    Se genero un evento en la GUI user para la creación o insertar usuarios a la DB.

--- src/main/java/views/UserController.java
+++ src/main/java/views/UserController.java
@@ -1,11 +1,14 @@
 package views;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import model.CRUD;
+import model.MD5;
+import static java.lang.Boolean.TRUE;
 
 public class UserController {
+
     @FXML
     private TextField tfName;
     @FXML
@@ -13,14 +16,39 @@ public class UserController {
     @FXML
     private TextField tfCC;
     @FXML
-    private TextField tfPwd;
+    private PasswordField pfPwd;
     @FXML
-    private TextField tfPwdConfirm;
+    private PasswordField pfPwdConfirm;
     @FXML
     private ComboBox<String> cbRol;
     @FXML
     private Button bCreate;
-    @FXML
-    private Button bOther;
 
+    @FXML
+    private void create(ActionEvent event) {
+        MD5 md5 = new MD5();
+        int cc = Integer.parseInt(tfCC.getText());
+        String name = tfName.getText();
+        String rol = "Administrador";
+        String pwd = pfPwd.getText();
+        String pwdC = pfPwdConfirm.getText();
+        String user = tfUser.getText();
+
+        if (pwd.equals(pwdC)) {
+            String encryptPwd = md5.encrypt(pwd);
+            CRUD.insertUser(cc,name,user,encryptPwd,rol,TRUE);
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setHeaderText(null);
+            alert.setTitle("Creación usuario");
+            alert.setContentText("El usuario "+ user + " fue creado");
+            alert.showAndWait();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText(null);
+            alert.setTitle("Creación usuario");
+            alert.setContentText("El usuario "+ user + " no se fue creado");
+            alert.showAndWait();
+        }
+    }
 }
