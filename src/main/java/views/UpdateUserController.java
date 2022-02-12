69dbc47970dc64fe2804a8a300ad8cbf62a65477
Victor <victor.sapuyes@correounivalle.edu.co>
2022-02-12 12:18:44 -0500

    Ventana update user
    crear updateUser.fxml y su controlador, cambios en colores

---
+++ src/main/java/views/UpdateUserController.java
@@ -0,0 +1,34 @@
+package views;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class UpdateUserController {
+
+    @FXML
+    private Label currentName;
+
+    @FXML
+    private Label currentUser;
+
+    @FXML
+    private Label currentCC;
+
+    @FXML
+    private Label currentRol;
+
+    @FXML
+    private TextField newUser;
+
+    @FXML
+    private TextField newName;
+
+    @FXML
+    private TextField newCC;
+
+    @FXML
+    private ComboBox newRol;
+
+}
