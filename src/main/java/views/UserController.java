476e065acdd607d57db5a978a3d1bae0af712551
Chris Sanchez <christian.valenzuela@correounivalle.edu.co>
2022-02-13 02:23:14 -0500

    Eventos GUI user y UserController
    Modifiacion a la GUI y se modifico el metodo create, se generaron eventos para los mensajes de error.

--- src/main/java/views/UserController.java
+++ src/main/java/views/UserController.java
@@ -1,14 +1,22 @@
 package views;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import model.CRUD;
 import model.MD5;
-import static java.lang.Boolean.TRUE;
+import model.User;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class UserController {
+public class UserController implements Initializable {
 
+    @FXML
+    private Label lError;
     @FXML
     private TextField tfName;
     @FXML
@@ -24,31 +32,67 @@ public class UserController {
     @FXML
     private Button bCreate;
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        initCB();
+    }
+
+    /**
+     * inicializa los valores en el ComboBox cbRol
+     */
+    public void initCB() {
+        ObservableList<String> option =
+                FXCollections.observableArrayList("Administrador", "Gerente", "Operador");
+        cbRol.setItems(option);
+    }
+
+    /**
+     * Verifica que todos los campos necesarios esten llenos
+     * @return boolean
+     */
+    public boolean validad() {
+        try {
+            Integer.parseInt(tfCC.getText());
+            tfName.getText();
+            cbRol.getSelectionModel().getSelectedItem();
+            pfPwd.getText();
+            pfPwdConfirm.getText();
+            tfUser.getText();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    @FXML
+    private void clean(MouseEvent event) {
+        lError.setText("");
+    }
+
     @FXML
     private void create(ActionEvent event) {
-        MD5 md5 = new MD5();
-        int cc = Integer.parseInt(tfCC.getText());
-        String name = tfName.getText();
-        String rol = "Administrador";
-        String pwd = pfPwd.getText();
-        String pwdC = pfPwdConfirm.getText();
-        String user = tfUser.getText();
+        if(validad()) {
 
-        if (pwd.equals(pwdC)) {
-            String encryptPwd = md5.encrypt(pwd);
-            CRUD.insertUser(cc,name,user,encryptPwd,rol,TRUE);
+            int cc = Integer.parseInt(tfCC.getText());
+            String name = tfName.getText();
+            String rol = cbRol.getSelectionModel().getSelectedItem();
+            String pwd = pfPwd.getText();
+            String pwdC = pfPwdConfirm.getText();
+            String user = tfUser.getText();
+            User usr = new User(cc, name, user, pwd, rol, true);
 
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setHeaderText(null);
-            alert.setTitle("Creación usuario");
-            alert.setContentText("El usuario "+ user + " fue creado");
-            alert.showAndWait();
+            if(usr.check(pwdC,pwd)) {
+                String encryptPwd = MD5.encrypt(pwd);
+                CRUD.insertUser(cc,name,user,encryptPwd,rol,true);
+            } else {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setHeaderText(null);
+                alert.setTitle("Creación usuario");
+                alert.setContentText("El usuario "+ user + " no se fue creado");
+                alert.showAndWait();
+            }
         } else {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setHeaderText(null);
-            alert.setTitle("Creación usuario");
-            alert.setContentText("El usuario "+ user + " no se fue creado");
-            alert.showAndWait();
+            lError.setText("Verifique que los campos esten llenos");
         }
     }
 }
