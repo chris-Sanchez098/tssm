062d52088274104422f9d75089b300f536fee7ae
edwin1007 <edwin.leonardo.cuaran@gmail.com>
2022-02-17 21:01:11 -0500

    creacion de BD y GUI para registrar clientes

---
+++ src/main/java/views/RegisterClientController.java
@@ -0,0 +1,86 @@
+package views;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class RegisterClientController {
+    @FXML
+    public TextField txtNameOp;
+    @FXML
+    public TextField txtIdentity;
+    @FXML
+    public TextField txtEmail;
+    @FXML
+    public TextField txtAddress;
+    @FXML
+    public TextField txtDate;
+    @FXML
+    public TextField txtTime;
+    @FXML
+    public TextField txtService;
+    @FXML
+    public TextField txtPhoneNumber;
+    @FXML
+    public TextField txtPayment;
+    @FXML
+    public TextField txtMinutes;
+    @FXML
+    public TextField txtMinutesUnlimited;
+    @FXML
+    public TextField txtData;
+    @FXML
+    public TextField txtGbCloud;
+    @FXML
+    public TextField txtGbShare;
+    @FXML
+    public TextField txtMsmUnlimited;
+    @FXML
+    public TextField txtNetflix;
+    @FXML
+    public TextArea txtAreaMoreInfo;
+    @FXML
+    public Button btnRegisterClient;
+    @FXML
+    public Button btnCancelRegister;
+    @FXML
+    public ComboBox<String> comboxSelectClient;
+    @FXML
+    public ComboBox<String> comboxSelectPlan;
+
+    @FXML
+    public void clickSelectClient(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    public void clickSelectPlan(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void clickRegisterClient(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void clickCancelRegister(ActionEvent actionEvent) {
+    }
+
+    public void CBSelectClient() {
+        ObservableList<String> option =
+                FXCollections.observableArrayList("", "Natural", "Corporativo");
+        comboxSelectClient.setItems(option);
+        comboxSelectClient.getSelectionModel().selectFirst();
+    }
+
+    public void CBSelectPlan() {
+        ObservableList<String> option =
+                FXCollections.observableArrayList("", "Plan 15 GB", "Plan 25 GB", "Plan 40 GB", "Plan ilimitado");
+        comboxSelectPlan.setItems(option);
+        comboxSelectPlan.getSelectionModel().selectFirst();
+    }
+}
