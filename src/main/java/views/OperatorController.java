5c3dc2ecd63c8a6d6ea89572cf94d6ca119a6c3f
edwin1007 <edwin.leonardo.cuaran@gmail.com>
2022-02-20 12:11:14 -0500

    se creo GUI de operador

--- src/main/java/views/OperatorController.java
+++ src/main/java/views/OperatorController.java
@@ -1,4 +1,37 @@
 package views;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class OperatorController {
+    @FXML
+    public Button btnInit;
+    @FXML
+    public Button btnPhonePlans;
+    @FXML
+    public Button btnRegisClient;
+    @FXML
+    public Button btnLogOut;
+    @FXML
+    public Button btnConsultClient;
+
+    @FXML
+    public void clickInit(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void clickPhonePlans(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void clickRegisClient(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void clickLogOut(ActionEvent actionEvent) {
+    }
+    @FXML
+    public void clickConsultClient(ActionEvent actionEvent) {
+    }
 }
